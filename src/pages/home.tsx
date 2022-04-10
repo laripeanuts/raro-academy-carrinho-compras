97d5e541ea37153663ef4a9ed980fae5aa7f1d6d
laripeanuts <larissarabelolf@gmail.com>
2022-04-10 20:31:52 -0300

    mostra lista de produtos na home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
-import Cart from "../components/Cart";
+//IMPORTE DE UTILIDADES
+import { useEffect, useState } from "react";
+import axios from "axios";
+//IMPORTE DE ESTILOS
 import { Container } from "../components/Container";
+//IMPORTE DE COMPONENTES
+import Cart from "../components/Cart";
 import Header from "../components/Header";
 import Product, { ProductProps } from "../components/Product";
+//IMPORTE DE TIPOS
+import { CartItemType } from "../types/CartItemType";
+import { useProducts } from "../contexts";
+
 
 const data: ProductProps = {
   id: 1,
@@ -12,18 +20,53 @@ const data: ProductProps = {
   price: 20.50,
 };
 
-const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
+const url = "http://localhost:3001/products";
+// const setProducts = async (): Promise<CartItemType[]> => {
+//   const response = await (
+//     await axios.get(url)
+//   ).data
 
-  return (
-    <>
-      <Header setIsOpen={setIsOpen} />
-      <Container>
-        <Product {...data} />
-        <Cart isOpen={isOpen} setIsOpen={setIsOpen} />
-      </Container>
-    </>
-  );
+//   return response;
+// };
+
+
+const Home = () => {
+  //ESTADOS GLOBAIS
+  const { products, setProducts } = useProducts();
+  //ESTADOS LOCAIS
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+  // const [products, setProducts] = useState([] as CartItemType[]);
+  
+  
+  const getTotalProducts = () => null;
+  const handleAddCart = () => null;
+  const handleDeleteCart = () => null;
+  
+  useEffect(() => {
+    setProducts();
+    setIsLoading(false);
+	}, []);
+
+	return (
+		<>
+			<Header setIsOpen={setIsOpen} />
+			<Container>
+				{isLoading ? (
+					<div>Carregando...</div>
+				) : (
+					<div>
+						Produtos carregados
+						{products.map((item: CartItemType) => {
+							return <Product key={item.id} {...item} />;
+						})}
+					</div>
+				)}
+				<Cart isOpen={isOpen} setIsOpen={setIsOpen} />
+			</Container>
+		</>
+	);
 };
 
 export default Home;
