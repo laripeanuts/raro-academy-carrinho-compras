966db640b99c2b3d67875e7f5e7baeda8069599b
laripeanuts <larissarabelolf@gmail.com>
2022-04-10 22:12:48 -0300

    adiciona progresso circular no loading

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,47 +6,30 @@ import { Container } from "../components/Container";
 //IMPORTE DE COMPONENTES
 import Cart from "../components/Cart";
 import Header from "../components/Header";
-import Product, { ProductProps } from "../components/Product";
+import LinearProgress from "@mui/material/CircularProgress";
+import Stack from "@mui/material/Stack";
+import { Box, CircularProgress } from "@mui/material";
 //IMPORTE DE TIPOS
 import { CartItemType } from "../types/CartItemType";
 import { useProducts } from "../contexts";
-
-
-const data: ProductProps = {
-  id: 1,
-  name: "Product 1",
-  picture:
-    "https://somos.lojaiplace.com.br/wp-content/uploads/2021/04/apple_iphone-12-spring21_purple_04202021.jpg",
-  price: 20.50,
-};
-
-const url = "http://localhost:3001/products";
-// const setProducts = async (): Promise<CartItemType[]> => {
-//   const response = await (
-//     await axios.get(url)
-//   ).data
-
-//   return response;
-// };
-
+import Product from "../components/Product";
 
 const Home = () => {
-  //ESTADOS GLOBAIS
-  const { products, setProducts } = useProducts();
-  //ESTADOS LOCAIS
-  const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [cartItems, setCartItems] = useState([] as CartItemType[]);
-  // const [products, setProducts] = useState([] as CartItemType[]);
-  
-  
-  const getTotalProducts = () => null;
-  const handleAddCart = () => null;
-  const handleDeleteCart = () => null;
-  
-  useEffect(() => {
-    setProducts();
-    setIsLoading(false);
+	//ESTADOS GLOBAIS
+	const { products, setProducts } = useProducts();
+	//ESTADOS LOCAIS
+	const [isOpen, setIsOpen] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	const [cartItems, setCartItems] = useState([] as CartItemType[]);
+	// const [products, setProducts] = useState([] as CartItemType[]);
+
+	const getTotalProducts = () => null;
+	const handleAddCart = () => null;
+	const handleDeleteCart = () => null;
+
+	useEffect(() => {
+		setProducts();
+		setIsLoading(false);
 	}, []);
 
 	return (
@@ -54,10 +37,13 @@ const Home = () => {
 			<Header setIsOpen={setIsOpen} />
 			<Container>
 				{isLoading ? (
-					<div>Carregando...</div>
+					<div className="loading">
+						<Box sx={{ display: "flex", justifyContent: "center" }}>
+							<LinearProgress disableShrink color="secondary" />
+						</Box>
+					</div>
 				) : (
 					<div>
-						Produtos carregados
 						{products.map((item: CartItemType) => {
 							return <Product key={item.id} {...item} />;
 						})}
